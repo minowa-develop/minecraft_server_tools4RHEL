76ff2229de241adcb484cd06814d0838dca920ee
mino-canon <mino_canon_0899@yahoo.co.jp>
2024-01-08 15:29:29 +0900

    バックアップ時の圧縮方法改修 #3
    -- 詳細 --
    現在7zipを使用しているが、7zipがない場合は他の圧縮方法(zip)を使用する

--- backup.sh
+++ backup.sh
@@ -10,4 +10,8 @@ TARGET_DIR="./minecraft_servers/${USE_SERVER}"
 CURRENT_DATE=`date "+%Y%m%d-%H%M%S"`
 BACKUPED_DIR="${CURRENT_DATE}_${USE_SERVER}"
 
-7za a -mx=9 -t7z "${BACKUPED_PATH}${BACKUPED_DIR}.7z" "$TARGET_DIR"
+if [ ${#ZIP7_HOME} -ne 0 ];then
+  7za a -mx=9 -t7z "${BACKUPED_PATH}${BACKUPED_DIR}.7z" "$TARGET_DIR"
+else
+  zip -9 "${BACKUPED_PATH}${BACKUPED_DIR}.zip" -r "$TARGET_DIR"
+fi
