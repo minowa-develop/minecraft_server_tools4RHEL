f7d7b491dfb296578a1309ce6dc923cec3067caa
mino-canon <mino_canon_0899@yahoo.co.jp>
2024-01-08 15:47:04 +0900

    javaのmcサーバ起動時にjavaのvaridateを行う #4

--- start_minecraft_server.sh
+++ start_minecraft_server.sh
@@ -7,6 +7,10 @@ cd "./minecraft_servers/${USE_SERVER}"
 
 # 種別を取得してサーバを開始する
 if [ -e "server.jar" ]; then
+  if [ ${#JAVA_PATH} -eq 0  ];then
+    echo "JAVA_PATHが設定されていません、common.confのJAVA_PATHを設定してください"
+    exit 1
+  fi
   ${JAVA_PATH}/java -Xmx1024M -Xms1024M -jar server.jar nogui
 else
   LD_LIBRARY_PATH=. ./bedrock_server
